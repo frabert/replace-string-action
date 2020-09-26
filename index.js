78c3257e41a7aa7f0531cddd1d6b5d0dfc8e6dab
Francesco Bertolaccini <francesco@bertolaccini.dev>
2020-09-26 20:19:01 +0200

    Revert "Adds testing and linting"
    This reverts commit 694bcff475fcb1eb34557fdb60a10099488dec67.
    
    PR #2 caused issue #3

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const core = require('@actions/core');
 try {
   const pattern = core.getInput('pattern');
   const string = core.getInput('string');
-  const replaceWith = core.getInput('replace-with');
-  const flags = core.getInput('flags');
+  const replaceWith = core.getInput('replace-with')
+  const flags = core.getInput('flags')
 
   const regex = new RegExp(pattern, flags);
 
